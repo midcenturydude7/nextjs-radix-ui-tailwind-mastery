27164f87a298a8993cee0f240838ef1407bbaac1
midCenturyDude7 <mgriffes@gmail.com>
2024-01-28 21:54:29 -0800

    Working on scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="bg-gray-800">Servers</div>
-      <div className="bg-gray-700">Channels</div>
-      <div className="bg-gray-600">Main</div>
+    <div className="flex h-screen text-white">
+      <div className="no-scrollbar space-y-2 overflow-y-scroll bg-gray-800 p-3">
+        {[...Array(40)].map((_, i) => (
+          <div
+            className="flex h-12 w-12 items-center justify-center rounded-full bg-white text-gray-800"
+            key={i}
+          >
+            {i}
+          </div>
+        ))}
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-white text-gray-800">
+          TW
+        </div>
+      </div>
+      <div className="flex w-60 flex-col bg-gray-700">
+        <div className="flex h-12 flex-shrink-0 items-center px-3 shadow-md">
+          Tailwind CSS
+        </div>
+        <div className="no-scrollbar flex-1 overflow-y-scroll p-3">
+          {[...Array(40)].map((_, i) => (
+            <p key={i}>channels {i}</p>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col bg-gray-600">
+        <div className="p-3 shadow-md">general</div>
+        <div className="flex-1 bg-black p-3">messages</div>
+      </div>
     </div>
   );
 }
