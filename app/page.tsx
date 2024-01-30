32ae411418fcede7a077eff3aaa75084558683e6
midCenturyDude7 <mgriffes@gmail.com>
2024-01-29 21:12:09 -0800

    Working with custom fonts and loading fonts

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         </div>
       </div>
       <div className="flex w-60 flex-col bg-gray-800">
-        <div className="flex h-12 items-center px-3 shadow-md">
+        <div className="font-title flex h-12 items-center px-3 text-white shadow-md">
           Tailwind CSS
         </div>
-        <div className="flex-1 space-y-2 overflow-y-scroll p-3 text-gray-300 no-scrollbar">
+        <div className="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300 no-scrollbar">
           <p className="text-white">channel (unread)</p>
           <p className="text-white">channel (unread)</p>
           {[...Array(40)].map((_, i) => (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +10,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Whitney", "Inter", ...defaultTheme.fontFamily.sans],
+        title: ["Ginto", "Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         gray: {
