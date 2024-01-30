5fb93533a8fbc77b109af55601d1472182fc2bfa
midCenturyDude7 <mgriffes@gmail.com>
2024-01-29 19:50:30 -0800

    Made nested panels scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen text-white">
-      <div className="no-scrollbar space-y-2 overflow-y-scroll bg-gray-800 p-3">
+      <div className="space-y-2 overflow-y-scroll bg-gray-800 p-3 no-scrollbar">
         {[...Array(40)].map((_, i) => (
           <div
             className="flex h-12 w-12 items-center justify-center rounded-full bg-white text-gray-800"
@@ -15,18 +15,27 @@ export default function Home() {
         </div>
       </div>
       <div className="flex w-60 flex-col bg-gray-700">
-        <div className="flex h-12 flex-shrink-0 items-center px-3 shadow-md">
+        <div className="flex h-12 items-center px-3 shadow-md">
           Tailwind CSS
         </div>
-        <div className="no-scrollbar flex-1 overflow-y-scroll p-3">
+        <div className="flex-1 space-y-2 overflow-y-scroll p-3 no-scrollbar">
           {[...Array(40)].map((_, i) => (
             <p key={i}>channels {i}</p>
           ))}
         </div>
       </div>
       <div className="flex flex-1 flex-col bg-gray-600">
-        <div className="p-3 shadow-md">general</div>
-        <div className="flex-1 bg-black p-3">messages</div>
+        <div className="flex h-12 items-center px-3 shadow-md">general</div>
+        <div className="flex-1 space-y-4 overflow-y-scroll p-3 no-scrollbar">
+          {[...Array(40)].map((_, i) => (
+            <p className="px-3" key={i}>
+              Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Quisquam maxime vero, nobis cupiditate animi totam minus,
+              amet sapiente omnis possimus laborum dolorem magnam obcaecati.
+              Voluptas, dolore. Natus placeat architecto praesentium.
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
